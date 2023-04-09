575a7227a2417c125e43212336e18233cb0d04e6
mellyexe <125124784+mellyexe@users.noreply.github.com>
2023-04-09 18:10:20 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,32 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+    T arr[size];
+    int top;
+public:
+    TStack(): top(-1) {}
+    void push(T value) {
+        if (isFull())
+            throw std::string("Stack is Full!");
+        else arr[++top] = value;
+    }
+    const T& pop() {
+        if (isEmpty())
+            throw std::string("Stack is Empty!");
+        else return arr[top--];
+    }
+    const T& peek() {
+        if (isEmpty())
+            throw std::string("Stack is Empty!");
+        else return arr[top];
+    }
+    bool isEmpty() const {
+        return -1 == top;
+    }
+    bool isFull() const {
+        return top == size - 1;
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
